afb3b3d75a6d301b68f7aaecbf86b91fff419e0b
Mirko Gonzalez Vasquez <mirko.gonzalez@xintec.cl>
2024-05-14 17:35:06 -0400

    wip: se agrega exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new HttpExceptionFilter(configService));
+  app.use(helmet());
+  await app.listen(configService.get<string>('port'));
 }
 bootstrap();
